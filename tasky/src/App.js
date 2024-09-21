990ee7a2d55621863c432cd0350bb2de48571671
theopakieser <20101918@mail.wit.ie>
2024-09-21 22:44:27 +0100

    Mostly done lab1

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" />
+      <p className='description'>Fold and put away </p>
+      <Task title="Laundry" deadline="Tomorrow"/>
+        <p className='description'>Fold and put away </p>
+      <Task title="Tidy" deadline="Today" />
     </div>
   );
 }
